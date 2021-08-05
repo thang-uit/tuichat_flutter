2f84040ff6e7e330b4c9358a064cb02f50d266a7
Chu Nam Thắng <namthangmu123@gamil.com>
2021-08-05 17:23:15 +0700

    17:23 5/8/2021

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFF5D442);
+const kSecondaryColor = Color(0xFFFE9901);
+const kContentColorLightTheme = Color(0xFF1D1D35);
+const kContentColorDarkTheme = Color(0xFFF5FCF9);
+const kWarningColor = Color(0xFFDBA800);
+const kErrorColor = Color(0xFFFF0000);
+
+const kDefaultPadding = 20.0;
---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:tuichat_flutter/constants.dart';
+
+class WelcomeScreen {
+}
