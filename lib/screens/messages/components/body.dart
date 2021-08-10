b446cb60cca5d38439700a18f3f3fedda0e7cfca
Chu Nam Thắng <namthangmu123@gamil.com>
2021-08-10 23:57:37 +0700

    23:57 10/8/2021

--- lib/screens/messages/components/body.dart
+++ lib/screens/messages/components/body.dart
@@ -9,25 +9,21 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: ListView.builder(
-                itemCount: chatMessages.length,
-                itemBuilder: (context, index) => Message(chatMessage: chatMessages[index]),
-              ),
+            child: ListView.builder(
+              itemCount: chatMessages.length,
+              itemBuilder: (context, index) => Message(chatMessage: chatMessages[index]),
             ),
         ),
         Container(
-          padding: EdgeInsets.symmetric(
-              horizontal: kDefaultPadding * 0.5, vertical: kDefaultPadding / 2),
+          padding: EdgeInsets.symmetric(horizontal: kDefaultPadding * 0.5, vertical: kDefaultPadding / 2),
           decoration: BoxDecoration(
             color: Theme.of(context).scaffoldBackgroundColor,
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 25),
-                  blurRadius: 30,
-                ),
-              ],
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 25),
+                blurRadius: 30,
+              ),
+            ],
           ),
           child: Row(
             children: [
@@ -88,33 +84,130 @@ class Message extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: this.chatMessage.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
-      children: [
-        TextMessage(chatMessage: this.chatMessage),
-      ],
+    Widget messageContent(ChatMessage chatMessage) {
+      switch (chatMessage.messageType) {
+        case ChatMessageType.text:
+          {
+            return TextMessage(chatMessage: chatMessage);
+            break;
+          }
+        case ChatMessageType.image:
+          {
+            return TextMessage(chatMessage: chatMessage);
+            break;
+          }
+        case ChatMessageType.video:
+          {
+            return TextMessage(chatMessage: chatMessage);
+            break;
+          }
+        case ChatMessageType.audio:
+          {
+            return AudioMessage(chatMessage: chatMessage);
+            break;
+          }
+
+        default:
+          {
+            return SizedBox();
+          }
+      }
+    }
+
+    return Container(
+      margin: EdgeInsets.fromLTRB(kDefaultPadding * 0.5 , kDefaultPadding * 0.5, kDefaultPadding * 0.5, 0),
+      child: Row(
+        mainAxisAlignment: this.chatMessage.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          if(!this.chatMessage.isSender) ... [
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage("assets/images/user_5.png"),
+            ),
+            SizedBox(width: kDefaultPadding / 4),
+          ],
+          messageContent(this.chatMessage),
+        ],
+      ),
     );
   }
 }
 
 class TextMessage extends StatelessWidget{
+  final ChatMessage chatMessage;
+
   const TextMessage({
     Key? key,
     required this.chatMessage,
   }): super(key: key);
 
-  final ChatMessage chatMessage;
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: kDefaultPadding * 0.5),
       padding: EdgeInsets.symmetric(horizontal: kDefaultPadding * 0.5, vertical: kDefaultPadding * 0.5),
       decoration: BoxDecoration(
         color: this.chatMessage.isSender ? kPrimaryColor : kNotSenderColor,
         borderRadius: BorderRadius.circular(20),
       ),
-      child: Text(this.chatMessage.text.toString().trim()),
+      child: Text(
+        this.chatMessage.text.toString().trim(),
+        style: TextStyle(color: kContentColorLightTheme),
+      ),
     );
   }
+}
+
+class AudioMessage extends StatelessWidget{
+  final ChatMessage chatMessage;
+
+  const AudioMessage({
+    Key? key,
+    required this.chatMessage,
+  }): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.55,
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding * 0.75, vertical: kDefaultPadding / 2.5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: this.chatMessage.isSender ? kPrimaryColor : kNotSenderColor,
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.play_arrow_rounded, color: Colors.black),
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding / 4),
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 2,
+                      color: Colors.black,
+                    ),
+                    Positioned(
+                      left: 0,
+                      child: Container(
+                        height: 8,
+                        width: 8,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ),
+          Text("0:20", style: TextStyle(fontSize: 12, color: Colors.black)),
+        ],
+      ),
+    );
+  }
+
 }
